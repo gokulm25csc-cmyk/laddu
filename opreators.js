1f02d064a45aa01a51d13ba9fa8daa1eb9820a13
Student <gokulm25csc@vetias.ac.in>
2026-06-12 14:01:02 +0530

    unrri and terrary

--- opreators.js
+++ opreators.js
@@ -23,7 +23,7 @@ console.log(a<=b)
 console.log(a>=b)
 console.log(a!=b)*/
 //logical opreator
-var a=20
+/*var a=20
 var b=40
 console.log(a<0 && b<0)
 console.log(a<0 && b<0)
@@ -32,4 +32,17 @@ console.log(!(a<0))
 console.log(a>0 && b>0)
 console.log(a>0 && b>0)
 console.log(a>0||b>0)
-console.log(!(a>0))
+console.log(!(a>0))*/
+//unarri opreatore
+var a=10
+console.log(a++)
+console.log(a--)
+console.log(++a)
+console.log(--a)
+//terrary operator
+
+var a=12
+var b=13
+console.log("ternary operator");
+var result = (a>b)? "a is greater ": "b is greater";
+console.log(result);

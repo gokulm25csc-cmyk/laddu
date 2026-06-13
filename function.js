e1f4fdd775e472e5ba4155bc830e050af4074fc3
Student <gokulm25csc@vetias.ac.in>
2026-06-13 10:52:28 +0530

    for and whillopping

--- function.js
+++ function.js
@@ -1,8 +1,13 @@
-var factor = "jhone";
+/*var factor = "jhone";
 var fplayer = "KBD SUDHAKAR";
 var fmovie = "VISHWASAM";
 
 function favorite() {
     return "My favorite actor is " + factor + ", my favorite player is " + fplayer + ", and my favorite movie is " + fmovie + ".";
 }
-console.log(favorite());
+console.log(favorite());*/
+if (score >= 90) {
+  console.log("Grade: A");
+} else {
+  console.log("Grade: B or below");
+}

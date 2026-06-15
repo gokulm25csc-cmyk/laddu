03a207848edc59b2d427d09878d4f0e405fce850
Student <gokulm25csc@vetias.ac.in>
2026-06-15 10:40:30 +0530

    conditional.js

---
+++ conditional.js
@@ -0,0 +1,16 @@
+var signal ="White"
+switch(signal){
+    case "green":
+    console.log("go!");
+    break;
+    case "yellow":
+    console.log("Wait!");
+    break;
+    case "red":
+    console.log("stop!");
+    break;
+    default:
+        console.log("Invalid color light");
+
+    
+}
